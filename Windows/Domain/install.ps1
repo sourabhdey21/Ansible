1f042686f771c17524ce49bf13c5279eef0a141d
Sourabh Dey <98477908+sourabhdey21@users.noreply.github.com>
2023-07-14 13:58:28 +0530

    Create install.ps1

---
+++ Windows/Domain/install.ps1
@@ -0,0 +1,22 @@
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Get-Command -Module ADDSDeployment
+#Declare variables
+$DatabasePath = "c:\windows\NTDS"
+$DomainMode = "WinThreshold"
+$DomainName = "homelab.com"
+$DomaninNetBIOSName = "HOMELAB"
+$ForestMode = "WinThreshold"
+$LogPath = "c:\windows\NTDS"
+$SysVolPath = "c:\windows\SYSVOL"
+$featureLogPath = "c:\poshlog\featurelog.txt" 
+$Password = (ConvertTo-SecureString -String "C1sc0123!" -AsPlainText -Force)
+#Install AD DS, DNS and GPMC 
+start-job -Name addFeature -ScriptBlock { 
+Add-WindowsFeature -Name "ad-domain-services" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "dns" -IncludeAllSubFeature -IncludeManagementTools 
+Add-WindowsFeature -Name "gpmc" -IncludeAllSubFeature -IncludeManagementTools } 
+Wait-Job -Name addFeature 
+
+    
+#Create New AD Forest
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath $DatabasePath -DomainMode $DomainMode -DomainName $DomainName -SafeModeAdministratorPassword $Password -DomainNetbiosName $DomainNetBIOSName -ForestMode $ForestMode -InstallDns:$true -LogPath $LogPath -NoRebootOnCompletion:$false -SysvolPath $SysVolPath -Force:$true
